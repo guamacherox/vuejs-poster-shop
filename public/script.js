47d180041e6c70db1332a8e05885553e99b67e85
juanlmarval <juan@retargetly.com>
2018-05-18 19:22:18 -0300

    preparing cart

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
-console.log('It works.');
+new Vue ({
+    el:'#app',
+    data:{
+        total: 50,
+        items: [
+            {title: 'Item 1'},
+            {title: 'Item 2'},
+            {title: 'Item 3'}
+        ],
+        cart: []
+    },
+    methods:{
+        addPrice: function() {
+            this.total += 9.99;
+        }
+    }
+});
