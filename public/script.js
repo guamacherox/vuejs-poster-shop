eaf997a7f4b4ad3e495273b9e782e0495decaf03
juanlmarval <juan@retargetly.com>
2018-05-22 10:49:48 -0300

    functionality for adding items to cart

--- public/script.js
+++ public/script.js
@@ -1,17 +1,28 @@
-new Vue ({
-    el:'#app',
-    data:{
+new Vue({
+    el: '#app',
+    data: {
         total: 50,
-        items: [
-            {title: 'Item 1'},
-            {title: 'Item 2'},
-            {title: 'Item 3'}
+        items: [{
+                id: 1,
+                title: 'Item 2'
+            },
+            {
+                id: 2,
+                title: 'Item 1'
+            },
+            {
+                id: 3,
+                title: 'Item 3'
+            }
         ],
         cart: []
     },
-    methods:{
-        addPrice: function() {
+    methods: {
+        addItem: function (index) {
             this.total += 9.99;
+            var item = this.items[index];
+
+            this.cart.push(item);
         }
     }
 });
